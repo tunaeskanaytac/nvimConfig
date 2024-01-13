a0200cbe32fdb61ceead42e67fc4c3a5a7eb9418
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-01-13 17:28:29 +0300

    Add LSP

--- lua/usr/prefs.lua
+++ lua/usr/prefs.lua
@@ -3,4 +3,6 @@ vim.cmd("set tabstop=2") -- space count
 vim.cmd("set softtabstop=2") -- acts if spaces are tab characters
 vim.cmd("set shiftwidth=2") -- indentation space
 vim.g.mapleader = ' ' -- set leader key as space
+vim.keymap.set("n", "<leader>fd", "<Cmd>Ex<CR>")
+vim.cmd("command Nohi nohlsearch")
 print("prefs loaded")
